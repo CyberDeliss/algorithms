6e8eb03f65981626e59dcf8c7294d9317f1fc675
CyberDeliss <ganyajob@gmail.com>
2022-12-14 02:19:50 +0400

    added names of functions

---
+++ my_sort_func.py
@@ -0,0 +1,43 @@
+"""
+1. bubble_sort O(n^2)
+2. mix_sorting (shaker sorting) O(n^2)
+3. comb_sorting
+4. insertion_sort O(n^2) O(n^2)
+5. selection_sort O(n^2)
+6. quick_sort O(n^2) -> O(n)
+7. merge_sort O(n log n)
+8. pyramid_sort O(n log n) -> O(n)
+9. tim_sort
+"""
+
+
+def bubble_sort(_list: list) -> list:
+    pass
+
+
+def mix_sorting(_list: list) -> list:
+    pass
+
+
+def comb_sorting(_list: list) -> list:
+    pass
+
+
+def insertion_sort(_list: list) -> list:
+    pass
+
+
+def selection_sort(_list: list) -> list:
+    pass
+
+
+def quick_sort(_list: list) -> list:
+    pass
+
+
+def merge_sort(_list: list) -> list:
+    pass
+
+
+def pyramid_sort(_list: list) -> list:
+    pass
